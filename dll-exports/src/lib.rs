1709ae2e7e08fc4b856ce99e659d2ca16e6802a8
Strackeror <rremilavergne@gmail.com>
2024-08-04 12:43:53 +0200

    Sort funcs

--- dll-exports/src/lib.rs
+++ dll-exports/src/lib.rs
@@ -5,11 +5,12 @@ use exe::{ExportDirectory, VecPE};
 
 pub fn get_exports(path: &Path) -> Result<Vec<String>> {
     let pe_file = VecPE::from_disk_file(path)?;
-    let vec = ExportDirectory::parse(&pe_file)?
+    let mut vec: Vec<String> = ExportDirectory::parse(&pe_file)?
         .get_export_map(&pe_file)?
         .into_keys()
         .map(|name| name.to_string())
         .filter(|n| n != "DllMain")
         .collect();
+    vec.sort();
     Ok(vec)
 }
